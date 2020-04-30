24459b2bba8a8f0098b60754070cc5ab71a306d9
Uzias Ferreira <uzias.frreira@zup.com.br>
2020-04-30 11:06:22 -0300

    Update previus tagy

--- fastlane/get_previous_tag.sh
+++ fastlane/get_previous_tag.sh
@@ -4,7 +4,7 @@ all_tags=`git tag -l | wc -l`
 
 if [ $all_tags = 0 ]  || [  $all_tags = 1 ]
 then
-  printf `git rev-list --max-parents=0 HEAD`
+  printf `git rev-list --max-parents=0 master`
 else
   printf `git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)`
 fi
